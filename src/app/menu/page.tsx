cd607223f73fe7c76e5ac56b9f323cc345e59e2c
Manickavasagam-81 <150415985+Manickavasagam-81@users.noreply.github.com>
2025-05-25 20:30:17 +0530

    build

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,6 +2,7 @@
 import { MenuType } from "@/types/types";
 import Link from "next/link";
 import React from "react";
+export const dynamic = "force-dynamic"; 
 const getData = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/categories`,{cache:"no-store"})
   if (!res.ok) {
