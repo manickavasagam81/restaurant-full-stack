c4b5585cf8822cd0b7862b5db9aed522be22f8d6
Manickavasagam-81 <150415985+Manickavasagam-81@users.noreply.github.com>
2025-05-25 21:06:35 +0530

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Featured from '@/components/Featured'
 import Offer from '@/components/Offer'
 import Slider from '@/components/Slider'
+export const dynamic = "force-dynamic";
 
 export default function Home() {
   return (
